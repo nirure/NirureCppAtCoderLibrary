e1a8fcb3d1c597a1aed269ef6cb15f6fbee69744
にるれぇ <153440975+nirure@users.noreply.github.com>
2024-12-09 22:33:53 +0900

    Update RLE(数字列).cpp

--- RLE(数字列).cpp
+++ RLE(数字列).cpp
@@ -3,8 +3,8 @@ vector<pair<int,int>>RLE(vector<int>S){
     int cnt=0;
     int c=S[0];
     for(int i=0;i<S.size();i++){
-        if(A[i]==c)cnt++;
-        else ans.push_back({c,cnt}),cnt=1,c=A[i];
+        if(S[i]==c)cnt++;
+        else ans.push_back({c,cnt}),cnt=1,c=S[i];
       }
     ans.push_back({c,cnt});
     return ans;
