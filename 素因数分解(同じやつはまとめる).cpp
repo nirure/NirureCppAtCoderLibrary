17105d3071feea1e6b98c240753104aada7583b6
にるれぇ <153440975+nirure@users.noreply.github.com>
2024-12-09 22:53:40 +0900

    Create 素因数分解(同じやつはまとめる).cpp

---
+++ 素因数分解(同じやつはまとめる).cpp
@@ -0,0 +1,10 @@
+vector<pair<long long,int>>prime(long long N){
+	vector<pair<long long,int>>ans;
+	for(long long i=2;i*i<=N;i++){
+		int cnt=0;
+		while(N%i==0)cnt++;
+		ans.push_back({i,cnt});
+	}
+	if(N!=1)ans.push_back({N,1});
+	return ans;
+}
