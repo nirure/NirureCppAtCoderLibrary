7619feea31a6d0ae51f6c1cdef7e05abe7169d3c
にるれぇ <153440975+nirure@users.noreply.github.com>
2024-12-09 22:33:22 +0900

    Update RLE(文字列).cpp

--- RLE(文字列).cpp
+++ RLE(文字列).cpp
@@ -1,11 +1,11 @@
 vector<pair<int,char>>RLE(string S){
-  vector<pair<int,char>>ans;
-  int cnt=0;
-  char c=S[0];
-  for(int i=0;i<N;i++){
-    if(A[i]==c)cnt++;
-    else ans.push_back(c,cnt),cnt=1,c=A[i];
-  }
-  ans.push_back({c,cnt});
-  return ans;
+    vector<pair<int,char>>ans;
+    int cnt=0;
+    char c=S[0];
+    for(int i=0;i<S.size();i++){
+        if(S[i]==c)cnt++;
+        else ans.push_back({c,cnt}),cnt=1,c=S[i];
+      }
+    ans.push_back({c,cnt});
+    return ans;
 }
