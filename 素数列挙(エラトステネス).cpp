1d47935a8ad2efa7d4a17681719e776b9a04dbf0
にるれぇ <153440975+nirure@users.noreply.github.com>
2024-12-09 22:36:49 +0900

    Create 素数列挙(エラトステネス).cpp
    せいぜい10^7程度にしておきましょう。(素数をとにかく見つけたいという人なら別ですが())

---
+++ 素数列挙(エラトステネス).cpp
@@ -0,0 +1,12 @@
+vector<ll> erat(int Max){
+	vector<ll>ans;
+	vector<bool>check(Max);
+	for(int i=2;i<Max;i++){
+		if(check[i])continue;
+		else{
+			ans.push_back(i);
+			for(int j=i*2;j<Max;j+=i)check[j]=1;
+		}
+	}
+	return ans;
+}
