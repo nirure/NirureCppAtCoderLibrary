ff712661fbb9d6e90ceea61cb7fb9dd3b7e5dd9e
にるれぇ <153440975+nirure@users.noreply.github.com>
2024-12-14 00:42:18 +0900

    Update 素因数分解(同じやつはまとめる).cpp

--- 素因数分解(同じやつはまとめる).cpp
+++ 素因数分解(同じやつはまとめる).cpp
@@ -2,7 +2,7 @@ vector<pair<long long,int>>prime(long long N){
 	vector<pair<long long,int>>ans;
 	for(long long i=2;i*i<=N;i++){
 		int cnt=0;
-		while(N%i==0)cnt++;
+		while(N%i==0)cnt++,N/=i;
 		ans.push_back({i,cnt});
 	}
 	if(N!=1)ans.push_back({N,1});
